865be2dfd80541d3ac541d1d2ac34a9cbfdc3faf
System administrator <root@ip-172-31-6-196.us-east-2.compute.internal>
2018-06-22 18:10:57 +0000

    updating

--- configuration.nix
+++ configuration.nix
@@ -8,15 +8,7 @@
       hydraURL = "http://ec2-18-222-119-244.us-east-2.compute.amazonaws.com"; # externally visible URL
       notificationSender = "hydra@holo.host"; # e-mail of hydra service
       # a standalone hydra will require you to unset the buildMachinesFiles list to avoid using a nonexistant /etc/nix/machines
-      buildMachines = [
-        { 
-          hostName = "localhost";
-          system = "x86_64-linux";
-          supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
-          maxJobs = 2;
-        }      
-
-      ];
+      
       # you will probably also want, otherwise *everything* will be built from scratch
       useSubstitutes = true;
       listenHost = "0.0.0.0";
@@ -27,4 +19,13 @@
     nginx.config = pkgs.lib.readFile /root/nginx.conf;
   };
   networking.firewall.allowedTCPPorts = [80 22];
+  nix.buildMachines = [
+        { 
+          hostName = "localhost";
+          system = "x86_64-linux";
+          supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
+          maxJobs = 2;
+        }      
+
+      ];
 }
